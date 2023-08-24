9b0df035e761d15f7b8f61b795d4abcac03d9b45
Sharanya <72227221+bsharanya-25@users.noreply.github.com>
2023-08-24 17:22:28 +0530

    Time: 5 ms (84.87%), Space: 42.8 MB (53.6%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,22 +1,24 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-       int n = nums.length;
-        int[][] dp = new int[n + 1][2001];  // Using 2001 to handle possible sums from -1000 to 1000
-
-        // Base case: There's one way to achieve sum 0, i.e., by not selecting any element
-        dp[0][1000] = 1;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < 2001; j++) {
-                if (j - nums[i - 1] >= 0) {
-                    dp[i][j] += dp[i - 1][j - nums[i - 1]];
-                }
-                if (j + nums[i - 1] <= 2000) {
-                    dp[i][j] += dp[i - 1][j + nums[i - 1]];
-                }
+        int sum = 0;
+        for(int x : nums)
+            sum += x;
+        if(((sum - target) % 2 == 1) || (target > sum))
+            return 0;
+        
+        int n = nums.length;
+        int s2 = (sum - target)/2;
+        int[][] t = new int[n + 1][s2 + 1];
+        t[0][0] = 1;
+        
+        for(int i = 1; i < n + 1; i++) {
+            for(int j = 0; j < s2 + 1; j++) {
+                if(nums[i - 1] <= j)
+                    t[i][j] = t[i-1][j] + t[i - 1][j - nums[i - 1]];
+                else
+                    t[i][j] = t[i - 1][j];
             }
         }
-
-        return dp[n][target + 1000];  // Adjust for the offset of -1000 to 1000 range
+        return t[n][s2];
     }
 }
