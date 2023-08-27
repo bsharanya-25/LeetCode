63720cce3763887446ccf9cebba95e1181ddcc88
Sharanya <72227221+bsharanya-25@users.noreply.github.com>
2023-08-27 20:05:35 +0530

    Time: 20 ms (49.24%), Space: 43.6 MB (37.34%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,15 +1,25 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-         int[] dp = new int[amount + 1];
-        Arrays.fill(dp, amount + 1); 
-        dp[0] = 0; 
-        
-        for (int coin : coins) {
-            for (int i = coin; i <= amount; i++) {
-                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+        int n = coins.length;
+        int t[][] = new int[n + 1][amount + 1];
+
+        for (int i = 0; i <= n; i++) {
+           for(int j=0;j<amount+1;j++){
+                if (i == 0) t[0][j] = Integer.MAX_VALUE - 1; // Initialize with a large value
+                if (j == 0) t[i][0] = 0;
             }
         }
         
-        return dp[amount] > amount ? -1 : dp[amount];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (coins[i - 1] <= j) {
+                    t[i][j] = Math.min(t[i - 1][j], 1 + t[i][j - coins[i - 1]]);
+                } else {
+                    t[i][j] = t[i - 1][j];
+                }
+            }
+        }
+        
+        return t[n][amount] == Integer.MAX_VALUE - 1 ? -1 : t[n][amount];
     }
 }
