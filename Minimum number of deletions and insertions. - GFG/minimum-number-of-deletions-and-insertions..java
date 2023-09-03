0da0da5fb68fb1712e1f1b45f768837842b777ad
Sharanya <72227221+bsharanya-25@users.noreply.github.com>
2023-09-03 23:41:44 +0530

    Added solution - LeetHub

--- Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..java
+++ Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..java
@@ -26,26 +26,18 @@ class Solution
 {
 	public int minOperations(String str1, String str2) 
 	{ 
-	    // Your code goes here
-	    int m = str1.length();
-        int n = str2.length();
-        
-        int[][] dp = new int[m + 1][n + 1];
-        
-            // Initialize the DP table
-        for (int i = 0; i <= m; i++) {
-            for (int j = 0; j <= n; j++) {
-                if (i == 0)
-                    dp[i][j] = j; // Insertion
-                else if (j == 0)
-                    dp[i][j] = i; // Deletion
-                else if (str1.charAt(i - 1) == str2.charAt(j - 1))
-                    dp[i][j] = dp[i - 1][j - 1];
-                else
-                    dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1]); // Insertion or Deletion
+	   
+        int n = str1.length();
+        int m = str2.length();
+        int[][] dp = new int[n+1][m+1];
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(str1.charAt(i-1) == str2.charAt(j-1)) dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
             }
         }
-        
-        return dp[m][n];
+        return (n - dp[n][m]) + (m - dp[n][m]); 
     }
+
 }
