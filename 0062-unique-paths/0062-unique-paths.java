3d8c32f049bb50e1b5fda42c97791920692e951b
Sharanya <72227221+bsharanya-25@users.noreply.github.com>
2023-09-07 19:44:47 +0530

    Time: 0 ms (100%), Space: 39.1 MB (78.55%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        // Create a 2D array to store the number of unique paths to each cell.
+        int[][] dp = new int[m][n];
+
+        // Initialize the leftmost column to 1 because there's only one way to reach any cell in the left column.
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+
+        // Initialize the top row to 1 because there's only one way to reach any cell in the top row.
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
+        }
+
+        // Fill in the DP array using dynamic programming.
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                // The number of unique paths to a cell is the sum of the paths from the cell above and the cell to the left.
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        // The value in the bottom-right cell represents the number of unique paths.
+        return dp[m - 1][n - 1];
+    }
+}
