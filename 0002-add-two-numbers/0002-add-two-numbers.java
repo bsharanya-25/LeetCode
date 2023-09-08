8073f8a366ed86c97e0ab15dafd4ce72a356d9fe
Sharanya <72227221+bsharanya-25@users.noreply.github.com>
2023-09-09 00:54:12 +0530

    Time: 1 ms (100%), Space: 43 MB (75.86%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,30 +10,27 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-              ListNode dummyHead = new ListNode(0);
-        ListNode tail = dummyHead;
-        int carry = 0;
+        ListNode dummyHead = new ListNode(0); // Create a dummy head for the result linked list
+        ListNode current = dummyHead;
+        int carry = 0; // Initialize carry to 0
 
-        while (l1 != null || l2 != null || carry != 0) {
-            int digit1 = (l1 != null) ? l1.val : 0;
-            int digit2 = (l2 != null) ? l2.val : 0;
+        while (l1 != null || l2 != null) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            int sum = x + y + carry;
 
-            int sum = digit1 + digit2 + carry;
-            int digit = sum % 10;
-            carry = sum / 10;
+            carry = sum / 10; // Calculate carry for the next iteration
+            current.next = new ListNode(sum % 10); // Add the sum's last digit to the result
+            current = current.next;
 
-            ListNode newNode = new ListNode(digit);
-            tail.next = newNode;
-            tail = tail.next;
-
-            l1 = (l1 != null) ? l1.next : null;
-            l2 = (l2 != null) ? l2.next : null;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
         }
 
-        ListNode result = dummyHead.next;
-        dummyHead.next = null;
-        return result;
-   
-        
+        if (carry > 0) {
+            current.next = new ListNode(carry); // If there's a carry left, add it as a new node
+        }
+
+        return dummyHead.next; // The actual result starts from the next node of the dummy head
     }
 }
