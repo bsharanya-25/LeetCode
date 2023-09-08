5e7bf4036768b3e33daab74e151d6804ae9565fc
Sharanya <72227221+bsharanya-25@users.noreply.github.com>
2023-09-08 13:13:53 +0530

    Time: 0 ms (100%), Space: 39.8 MB (9.13%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,26 +1,24 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
+         int[][] dp = new int[m][n];
 
-        // Initialize the leftmost column to 1 because there's only one way to reach any cell in the left column.
+        // Initialize the bottom row and rightmost column to 1,
+        // as there's only one way to reach any cell in those rows/columns.
         for (int i = 0; i < m; i++) {
-            dp[i][0] = 1;
+            dp[i][n - 1] = 1;
         }
-
-        // Initialize the top row to 1 because there's only one way to reach any cell in the top row.
         for (int j = 0; j < n; j++) {
-            dp[0][j] = 1;
+            dp[m - 1][j] = 1;
         }
 
-        // Fill in the DP array using dynamic programming.
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                // The number of unique paths to a cell is the sum of the paths from the cell above and the cell to the left.
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        // Fill in the DP table by summing the paths from below and from the right.
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
             }
         }
 
-        // The value in the bottom-right cell represents the number of unique paths.
-        return dp[m-1][n-1];
+        // The value in dp[0][0] represents the total unique paths.
+        return dp[0][0];
     }
 }
