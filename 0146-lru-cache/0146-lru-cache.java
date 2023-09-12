79ea33ddb6f25c3d15a1f66df6ed97d73834acf5
Sharanya <72227221+bsharanya-25@users.noreply.github.com>
2023-09-12 22:16:51 +0530

    Time: 41 ms (87.32%), Space: 109.8 MB (96.42%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,92 +1,75 @@
 class LRUCache {
-    class DlNode{
+    class DlNode {
         int key, value;
-        DlNode prev=null, next=null;
-        DlNode(){}
+        DlNode prev, next;
+
+        DlNode() {
+            prev = next = null;
+        }
+
         DlNode(int key, int value) {
             this.key = key;
             this.value = value;
+            prev = next = null;
         }
     }
 
-    private HashMap<Integer,DlNode> cache;
-    private int cap;
-    private int count;
+    private Map<Integer, DlNode> cache;
+    private int capacity;
+    private int size;
     private DlNode head, tail;
 
-    private void addNode(DlNode newNode) {
-        DlNode nxt = head.next;
-
-        newNode.prev = head;
-        newNode.next = nxt;
-        head.next = newNode;
-        nxt.prev = newNode;
+    private void addNode(DlNode node) {
+        node.prev = head;
+        node.next = head.next;
+        head.next.prev = node;
+        head.next = node;
     }
 
     private void removeNode(DlNode node) {
-       DlNode pre = node.prev;
-        DlNode nxt = node.next;
-        pre.next = nxt;
-        nxt.prev = pre; 
-    }
-    private DlNode delFromTail() {
-        DlNode preTail = tail.prev;
-        
-        tail.prev = preTail.prev;
-        preTail.prev.next = tail;
-        
-        return preTail;
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
     }
 
-    private void moveToFront(DlNode node) {
-        this.removeNode(node);
-        this.addNode(node);
+    private DlNode removeTail() {
+        DlNode tailPrev = tail.prev;
+        removeNode(tailPrev);
+        return tailPrev;
     }
 
     public LRUCache(int capacity) {
         cache = new HashMap<>();
-        this.cap = capacity;
-        this.count = 0;
+        this.capacity = capacity;
+        size = 0;
         head = new DlNode();
         tail = new DlNode();
-
         head.next = tail;
         tail.prev = head;
     }
-    
+
     public int get(int key) {
-       DlNode res = this.cache.get(key);
-       if(res == null) return -1;
-
-       this.moveToFront(res);
-       return res.value;
+        DlNode node = cache.get(key);
+        if (node == null) return -1;
+        removeNode(node);
+        addNode(node);
+        return node.value;
     }
-    
+
     public void put(int key, int value) {
-        DlNode res = this.cache.get(key);
-
-        if(res == null) {
-            DlNode newNode = new DlNode(key, value);
-            this.cache.put(key, newNode);
-            this.addNode(newNode);
-            ++count;
-
-            if(count > cap) {
-                DlNode del = this.delFromTail();
-                this.cache.remove(del.key);
-                --count;
+        DlNode node = cache.get(key);
+        if (node == null) {
+            if (size >= capacity) {
+                DlNode removed = removeTail();
+                cache.remove(removed.key);
+                size--;
             }
-
+            node = new DlNode(key, value);
+            cache.put(key, node);
+            size++;
         } else {
-            res.value = value;
-            this.moveToFront(res);
+            node.value = value;
+            removeNode(node);
         }
+        addNode(node);
     }
 }
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
