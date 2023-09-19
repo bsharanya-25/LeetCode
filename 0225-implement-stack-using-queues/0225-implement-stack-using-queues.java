a69b81c1de8e5faf8dadde61cf6a25e0fea14b00
Sharanya <72227221+bsharanya-25@users.noreply.github.com>
2023-09-19 14:59:44 +0530

    Time: 0 ms (100%), Space: 40.5 MB (13.7%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -0,0 +1,36 @@
+
+class MyStack {
+    private Queue<Integer> queue;
+
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        int size = queue.size();
+        queue.offer(x);
+
+        // Rotate the elements to maintain the order like a stack
+        for (int i = 0; i < size; i++) {
+            queue.offer(queue.poll());
+        }
+    }
+
+    public int pop() {
+        if (empty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return queue.poll();
+    }
+
+    public int top() {
+        if (empty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return queue.peek();
+    }
+
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
