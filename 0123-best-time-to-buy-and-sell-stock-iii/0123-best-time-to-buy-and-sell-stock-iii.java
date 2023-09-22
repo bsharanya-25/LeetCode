7d0bb35e66153094aaff1b9674efe8a756e1166a
Sharanya <72227221+bsharanya-25@users.noreply.github.com>
2023-09-22 15:19:39 +0530

    Time: 4 ms (75.91%), Space: 55.7 MB (87.74%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,23 +1,17 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if (prices.length <= 1) {
-            return 0; // If there are no prices or only one price, we can't make any profit.
-        }
         int buy1 = Integer.MIN_VALUE;
         int buy2 = Integer.MIN_VALUE;
         int sell1 = 0;
         int sell2 = 0;
 
-        for (int price : prices) {
-    
-            buy1 = Math.max(buy1, -price);
-            sell1 = Math.max(sell1, buy1 + price);
+        for(int  i : prices){
+            buy1 = Math.max(buy1, -i);
+            sell1 = Math.max(sell1 , buy1 + i);
 
-            // Second buy and sell
-            buy2 = Math.max(buy2, sell1 - price);
-            sell2 = Math.max(sell2, buy2 + price);
+            buy2 = Math.max(buy2, sell1 - i);
+            sell2 = Math.max(sell2, buy2 + i);
         }
-
-        return sell2;
+    return sell2;
     }
 }
