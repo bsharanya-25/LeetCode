16370737ef9cd0076e6fae2a14d8cec3de1918a4
Sharanya <72227221+bsharanya-25@users.noreply.github.com>
2023-09-30 19:29:47 +0530

    Time: 15 ms (48.6%), Space: 42.6 MB (88.85%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,27 +1,27 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-        int closestSum = nums[0] + nums[1] + nums[2]; // Initialize closest sum with the sum of the first three elements
+        int closestSum = Integer.MAX_VALUE;
+        //int closestSum = nums[0] + nums[1] + nums[2]; -> Initialize closest sum with the sum of the first three elements
 
         for (int i = 0; i < nums.length - 2; i++) {
-            int j = i + 1;
-            int k = nums.length - 1;
+            int left = i + 1;
+            int right = nums.length - 1;
 
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
+            while (left < right) {
+                int currentSum = nums[i] + nums[left] + nums[right];
 
-                if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
-                    closestSum = sum; // Update closest sum if the current sum is closer to the target
+                if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+                    closestSum = currentSum;
                 }
 
-                if (sum < target) {
-                    j++; // Increment j to increase the sum
+                if (currentSum < target) {
+                    left++;
                 } else {
-                    k--; // Decrement k to decrease the sum
+                    right--;
                 }
             }
         }
-
         return closestSum;
     }
 }
