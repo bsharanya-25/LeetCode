4e77be986885c569410be7f10a5f6277af22eb6a
Sharanya <72227221+bsharanya-25@users.noreply.github.com>
2023-10-03 17:19:50 +0530

    Time: 10 ms (97.03%), Space: 41.2 MB (63.22%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
@@ -1,22 +1,33 @@
 class Solution {
     public int minCost(int n, int[] cuts) {
+        // Sort the cut positions in ascending order
         Arrays.sort(cuts);
+        
+        // Get the number of cut positions
         int m = cuts.length;
+        
+        // Create a 2D DP array to store the minimum cost of cutting
         int[][] dp = new int[m + 2][m + 2];
-
+        
+        // Loop for different segment lengths
         for (int l = 2; l <= m + 1; l++) {
             for (int i = 0; i + l <= m + 1; i++) {
                 int j = i + l;
                 dp[i][j] = Integer.MAX_VALUE;
+                
+                // Find the optimal cut position within the segment
                 for (int k = i + 1; k < j; k++) {
                     dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k][j]);
                 }
+                
+                // Calculate the cost of cutting the segment
                 int left = (i == 0) ? 0 : cuts[i - 1];
                 int right = (j == m + 1) ? n : cuts[j - 1];
                 dp[i][j] += right - left;
             }
         }
 
+        // The result is stored in dp[0][m + 1], which represents the minimum cost
         return dp[0][m + 1];
     }
 }
