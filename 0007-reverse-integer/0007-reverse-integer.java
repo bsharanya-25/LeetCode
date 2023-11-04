7ef793dfa16e37e9c252f7ca7c50f7649c5cb274
Sharanya <72227221+bsharanya-25@users.noreply.github.com>
2023-11-04 21:59:53 +0530

    Time: 1 ms (97.78%), Space: 40.1 MB (17.02%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int reverse(int x) {
+        long finalNum = 0;
+        while(x!=0){
+            int lastDig = x%10; // extract last digit from the digits
+            finalNum = (finalNum + lastDig)*10; 
+            x= x/10; // remove the extracted digit from the digits
+        }
+        
+        finalNum = finalNum/10;
+        if(finalNum > Integer.MAX_VALUE || finalNum<Integer.MIN_VALUE){
+            return 0;
+        }
+        if(x<0){
+            return (int)(-1*finalNum);
+        }
+        return (int)finalNum;
+    }
+}
