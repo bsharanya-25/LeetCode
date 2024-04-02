43fd8889cbe4fbc76965baedcfda1e49fc248817
Sharanya <72227221+bsharanya-25@users.noreply.github.com>
2024-04-02 21:12:57 +0530

    Time: 11 ms (58.83%) | Memory: 42.3 MB (67.12%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Character> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char charS = s.charAt(i);
+            char charT = t.charAt(i);
+
+            if (map.containsKey(charS)) {
+                if (map.get(charS) != charT) {
+                    return false;
+                }
+            } else {
+                // Check if the character in t has already been mapped to another character
+                if (map.containsValue(charT)) {
+                    return false;
+                }
+                map.put(charS, charT);
+            }
+        }
+
+        return true;
+    }
+}
