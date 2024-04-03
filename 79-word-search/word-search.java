a75f5bd38202aa3bd951c5dca96d2ae8c88c5482
Sharanya <72227221+bsharanya-25@users.noreply.github.com>
2024-04-03 21:09:51 +0530

    Time: 138 ms (70.43%) | Memory: 41.4 MB (74.16%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -19,28 +19,25 @@ class Solution {
 
     // Depth-first search (DFS) to traverse the board
     private boolean dfs(char[][] board, int i, int j, String word, int index) {
-        // If all characters in the word are found, return true
         if (index == word.length())
             return true;
-            int rows = board.length;
+
+        int rows = board.length;
         int cols = board[0].length;
 
-        // If the current cell is out of bounds or doesn't match the current character of the word, return false
         if (i < 0 || i >= rows || j < 0 || j >= cols || board[i][j] != word.charAt(index))
             return false;
 
         char temp = board[i][j];
-        board[i][j] = '*'; // Mark the current cell as visited
+        board[i][j] = '*'; // Modify the cell temporarily to indicate visited status
 
-        // Explore adjacent cells recursively to find the next character of the word
         boolean found = dfs(board, i + 1, j, word, index + 1) ||
                         dfs(board, i - 1, j, word, index + 1) ||
                         dfs(board, i, j + 1, word, index + 1) ||
                         dfs(board, i, j - 1, word, index + 1);
 
-        board[i][j] = temp; // Restore the original value of the current cell
+        board[i][j] = temp; // Restore the original value of the cell
         return found;
-
 
     }
 }
