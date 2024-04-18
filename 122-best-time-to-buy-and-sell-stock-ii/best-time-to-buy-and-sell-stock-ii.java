832044833f036520a78b1ed184d13f4277e7b324
Sharanya <72227221+bsharanya-25@users.noreply.github.com>
2024-04-18 23:15:23 +0530

    Time: 1 ms (93.19%) | Memory: 45.9 MB (30.11%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,32 @@
+// DP Approach
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int n = prices.length;
+//         int dp[] = new int[n];
+
+//         for(int i = 1;i < n; i++){
+//             if(prices[i]>prices[i-1]){
+//                 dp[i] = dp[i-1] +(prices[i] - prices[i-1]);
+//             }else{
+//                 dp[i] = dp[i-1];
+//             }
+//         }
+//         return dp[n-1];
+//     }
+// }
+//TC:o(n) and sc:O(n)
+
+
+// Greedy approach
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i-1] < prices[i]) {
+                maxProfit += prices[i] - prices[i - 1];
+            }
+        }
+        return maxProfit;
+    }
+}
+//TC:o(n) and sc:O(1)
