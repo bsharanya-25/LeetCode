13817ca5ae67804b1679c343219991cabce8e748
Sharanya <72227221+bsharanya-25@users.noreply.github.com>
2024-04-21 01:43:19 +0530

    Time: 50 ms (81.47%) | Memory: 61.2 MB (53.64%) - LeetSync

--- 220-contains-duplicate-iii/contains-duplicate-iii.java
+++ 220-contains-duplicate-iii/contains-duplicate-iii.java
@@ -1,8 +1,11 @@
+import java.util.HashMap;
+
 class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
         if (valueDiff < 0) {
             return false;
         }
+        
         long width = (long) valueDiff + 1;
         HashMap<Long, Long> buckets = new HashMap<>();
         
@@ -10,6 +13,7 @@ class Solution {
             long num = nums[i];
             long id = num < 0 ? (num + 1) / width - 1 : num / width;
             
+            // Check if the same bucket or nearby buckets have elements within the value difference
             if (buckets.containsKey(id)
                 || (buckets.containsKey(id - 1) && num - buckets.get(id - 1) <= valueDiff)
                 || (buckets.containsKey(id + 1) && buckets.get(id + 1) - num <= valueDiff)) {
@@ -18,9 +22,10 @@ class Solution {
             
             buckets.put(id, num);
             
+            // Remove elements from buckets that fall out of the window
             if (i >= indexDiff) {
-                long old_id = nums[i - indexDiff] < 0 ? (nums[i - indexDiff] + 1) / width - 1 : nums[i - indexDiff] / width;
-                buckets.remove(old_id);
+                long oldId = nums[i - indexDiff] < 0 ? (nums[i - indexDiff] + 1) / width - 1 : nums[i - indexDiff] / width;
+                buckets.remove(oldId);
             }
         }
         
